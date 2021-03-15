295f99f3dea9f2fc9ba4ffd911ba9885c2568a36
Ognjen <ognjenrajkovic96@gmail.com>
2021-03-15 18:01:54 +0100

    fix bugs for travel method

--- src/main/java/main/car/Car.java
+++ src/main/java/main/car/Car.java
@@ -6,27 +6,53 @@ public class Car {
     public String model;
     public String color;
     private int buildYear;
+    private int mileage = 0;
+    private int fuel;
+    private int consumption = 5;
     
     public Car() {
-        
+        this.brand = "";
+        this.model = "";
+        this.color = "";
+        this.buildYear = 0;
     }
     
-    
-    public Car(int year) {
-        this.buildYear = year;
+    public Car(int buildYear) {
+        this.buildYear = buildYear;
     }
-   
-    public Car(String brand, String model, String color, int buildYear) {
+    
+    public Car(String brand, String model, String color, int buildYear, int mileage, int fuel, int consumption) {
         this.brand = brand;
         this.model = model;
         this.color = color;
         this.buildYear = buildYear;
-        
-                
+        this.mileage = mileage;
+        this.fuel = fuel;
+        this.consumption = consumption;
     }
     
     public int getBuildYear() {
         return this.buildYear;
+    }
+    
+    private int getMileage() {
+        return this.mileage;
+    }
+    
+    private void setMileage(int newMileage) {
+        this.mileage = newMileage;
+    }
+    
+    private int getConsumption() {
+        return this.consumption;
+    }
+    
+    private int getFuel() {
+        return this.fuel;
+    }
+    
+    private void setFuel(int fuel) {
+        this.fuel = fuel;
     }
     
     public void printAttributes() {
@@ -34,6 +60,26 @@ public class Car {
         System.out.println("Model: " + this.model);
         System.out.println("Color: " + this.color);
         System.out.println("Build Year: " + this.getBuildYear());
+        System.out.println("Mileage: " + this.getMileage());
+        System.out.println("Current fuel: " + this.getFuel());
+        System.out.println("");
     }
+    
+    public void travel(int distance) {
+        int fuelNeededForTrip = this.getConsumption() * distance / 100;
+        if (this.getFuel() >= fuelNeededForTrip) {
+            this.setMileage(this.getMileage() + distance);
+            this.setFuel(this.getFuel() - fuelNeededForTrip);
+            System.out.println("Uspešno je pređen put od " + distance + " kilometara");
+        } else {
+            System.out.println("Nema dovoljno goriva za takav put");
+        }
+    }
+    
+    // public fuelUp(int amountOfFuel)
+    // maxFuel (kapacitet rezervoara)
+    
+        
+    
     
 }
--- src/main/java/main/car/Main.java
+++ src/main/java/main/car/Main.java
@@ -2,34 +2,45 @@ package main.car;
 
 
 public class Main {
-   public static void main(String[] args) {
-       int a = 5;
-       int b = 6;
-       Car audi = new Car();
-       audi.brand = "Audi";
-       audi.model = "A4";
-       audi.color = "black";
-       //audi.buildYear = 2005;
-       
-       System.out.println("Brand: " + audi.brand);
-       System.out.println("Model: " + audi.model);
-       System.out.println("Model: " + audi.getBuildYear());
-       
-       
-       Car bmw = new Car();
-       bmw.brand = "BMW";
-       bmw.model = "X5";
-       
-       System.out.println("Brand: " + bmw.brand);
-       System.out.println("Model: " + bmw.model);
-       
-       
-       Car fiat = new Car(2010);
-       System.out.println("Godina proizvodnje " + fiat.getBuildYear());
-       
-       Car lada = new Car("Lada", "Niva", "green", 2018);
-       lada.printAttributes();
-   
-}
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        Car audi = new Car();
+        audi.printAttributes();
+        audi.brand = "Audi";
+        audi.model = "A4";
+        audi.color = "black";
+        audi.printAttributes();
+        audi.travel(150);
+        audi.printAttributes();
+//        audi.fuelUp(30000);
+//        audi.travel(300);
+        
+        
+        Car bmw = new Car();
+        bmw.brand = "BMW";
+        bmw.model = "X5";
+        bmw.printAttributes();
+        
+        Car fiat = new Car(2010);
+        System.out.println("Godina proizvodnje: " + fiat.getBuildYear());
+        
+        Car lada = new Car("Lada", "Niva", "green", 2018, 0, 50, 10);
+        lada.printAttributes();
+//        lada.setFuel(343);
+        //proveriti gorivo, 
+
+        lada.travel(300);
+        lada.printAttributes();
+        lada.travel(200);
+        lada.printAttributes();
+        lada.travel(100);
+        lada.printAttributes();
+        
+
+        
+    }
+
     
 }
